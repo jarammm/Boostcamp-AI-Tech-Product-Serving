dd7ad12550ce02ef8b80e655ba69d51df6e78ea1
jarammm <jaramrammm@gmail.com>
2022-05-30 14:00:50 +0000

    doc: reload에 대한 설명 추가

--- part3/01-fastapi/app/__main__.py
+++ part3/01-fastapi/app/__main__.py
@@ -1,4 +1,5 @@
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8001, reload=True)
+    # uvicorn.run("app.main:app", host="127.0.0.1", port=8001, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=30002, reload=True)  # reload=True : 새로운 정보가 입력될 때마다, 변화가 발생할 때마다 다시 로딩됨
